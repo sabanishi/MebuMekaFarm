3cd9c89fa70d1767f8c190e8f1ecf63b1dde5660
Sabanishi <84651801+sabanishi@users.noreply.github.com>
2026-09-11 01:14:19 +0900

    SingletonMonoBehavior導入

---
+++ Assets/Scripts/Common/SingletonMonoBehaviour.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+namespace Sabanishi.MebuMekaFarm.Common
+{
+    public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        public static T Instance { get; private set; }
+
+        protected void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = GetComponent<T>();
+                OnAwakeInternal();
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        protected void OnDestroy()
+        {
+            if (Instance == this)
+            {
+                OnDestroyInternal();
+                Instance = null;
+            }
+        }
+        
+        /// <summary>
+        /// Awake時に実行されるメソッド
+        /// </summary>
+        protected virtual void OnAwakeInternal() { }
+        
+        /// <summary>
+        /// Destroy時に実行されるメソッド
+        /// </summary>
+        protected virtual void OnDestroyInternal() { }
+    }
+}
